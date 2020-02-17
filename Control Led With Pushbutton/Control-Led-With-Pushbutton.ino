8356699beb86475e242386c3bdd224104dd90083
Aiversity <61116115+Aiversity@users.noreply.github.com>
2020-02-17 18:55:00 +0530

    Update Control-Led-With-Pushbutton.ino

--- Control Led With Pushbutton/Control-Led-With-Pushbutton.ino
+++ Control Led With Pushbutton/Control-Led-With-Pushbutton.ino
@@ -1,1 +1,22 @@
+int Button;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Button = analogRead(A0);
+  Serial.begin(9600);
+}
 
+void loop() {
+  // put your main code here, to run repeatedly:
+  Button = analogRead(A0);
+  delay(500);
+  Serial.println(Button);
+  if (Button >= 1000){
+      digitalWrite(13, 0);
+
+  }
+  else{
+      digitalWrite(13, 1);
+
+  }
+}
