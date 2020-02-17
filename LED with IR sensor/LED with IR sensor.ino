7b29a01675353d264354c4c18e8d38e470667033
Aiversity <61116115+Aiversity@users.noreply.github.com>
2020-02-17 20:28:02 +0530

    Update LED with IR sensor.ino

--- LED with IR sensor/LED with IR sensor.ino
+++ LED with IR sensor/LED with IR sensor.ino
@@ -1,1 +1,21 @@
 
+int IR;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  IR = analogRead(A0);
+  Serial.println(IR);
+  if(IR <= ){ //Change the 2nd value according to your highest value. Check it with the test.ino code given in the previous folder
+      digitalWrite(13, 0);
+
+  }
+  else{
+      digitalWrite(13, 1);
+
+  }
+}
