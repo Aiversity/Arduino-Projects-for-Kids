55f1240f08971f5f6a7aa59ff338a02bc5586e18
Aiversity <61116115+Aiversity@users.noreply.github.com>
2020-02-18 19:34:24 +0530

    Create LIght sensor and Led.ino

---
+++ Control Led With Light Sensor/LIght sensor and Led.ino
@@ -0,0 +1,19 @@
+int Light;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Light = analogRead(A0);
+  Serial.println(Light);
+  if(Light >= 870){
+    digitalWrite(13, 1);
+  }
+  else{
+    digitalWrite(13, 0);
+  }
+}
