4799a167172818a823926864eb3fb34ccbcb893b
Aiversity <61116115+Aiversity@users.noreply.github.com>
2020-07-04 14:56:40 +0530

    Update Traffic_Lights.ino

--- Traffic Lights/Traffic_Lights.ino
+++ Traffic Lights/Traffic_Lights.ino
@@ -1,1 +1,43 @@
+#define Red_pin 8
+#define Yellow_pin 10
+#define Green_pin 12
 
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(Red_pin, OUTPUT);
+  pinMode(Yellow_pin, OUTPUT);
+  pinMode(Green_pin, OUTPUT);
+
+}
+
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(Red_pin, HIGH);   
+  delay(3000);               
+  digitalWrite(Yellow_pin, HIGH);    
+  delay(1000);
+  digitalWrite(Red_pin, LOW);    
+  digitalWrite(Yellow_pin, LOW);    
+  digitalWrite(Green_pin, HIGH);
+  delay(3000);
+
+  //Green blinking    
+  delay(500);
+  digitalWrite(Green_pin, HIGH);    
+  delay(500);
+  digitalWrite(Green_pin, LOW);    
+  delay(500);
+  digitalWrite(Green_pin, HIGH);    
+  delay(500);
+  digitalWrite(Green_pin, LOW);    
+  delay(500);
+  digitalWrite(Green_pin, HIGH);    
+  delay(500);
+  digitalWrite(Green_pin, LOW);
+
+  digitalWrite(Yellow_pin, HIGH);
+  delay(1000);
+  digitalWrite(Yellow_pin, LOW);
+}
